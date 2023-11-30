bcba2994bfa0e5aea532dca977cb55eac00f223b
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-11-30 17:07:48 +0900

    fix:  workfolderの指定漏れ修正

--- test/runTest.js
+++ test/runTest.js
@@ -12,8 +12,14 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
+    const worspaceFolder = path.resolve(__dirname, "./assets/sample");
+
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [worspaceFolder],
+    });
   } catch (err) {
     console.error("Failed to run tests", err);
     process.exit(1);
